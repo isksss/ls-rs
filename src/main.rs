35d94f37f552e9c209b9924549d3c2986835007a
isksss <104404522+isksss@users.noreply.github.com>
2023-11-02 10:10:18 +0900

    v0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::fs;
+use std::path::Path;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    // target
+    let args : Vec<String> = env::args().collect();
+    let target : &str;
+    if args.len() < 2 {
+        target = ".";
+    }else{
+        target = &args[1];
+    }
+    
+
+    // check if target exists
+    if !Path::new(target).exists() {
+        println!("{} does not exist", target);
+        return;
+    }
+    // check if target is a directory
+    if !Path::new(target).is_dir() {
+        println!("{} is not a directory", target);
+        return;
+    }
+    
+    // read directory
+    if let Ok(entries) = fs::read_dir(target){
+        for entry in entries {
+            if let Ok(entry) = entry {
+                println!("{:?}", entry.path());
+            }
+        }
+    }
 }
